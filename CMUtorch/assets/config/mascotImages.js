3f143eaf6b698f8c97f101440fb3d518e44f42f1
kantapong-seasky <kuroka151725@gmail.com>
2025-11-17 05:24:02 +0700

    mascot01

---
+++ CMUtorch/assets/config/mascotImages.js
@@ -0,0 +1,26 @@
+export const mascotImages = {
+    orange: {
+        good: require('../../assets/mascot/orange/good/torch_orange_good.png'),
+        sad: require('../../assets/mascot/orange/sad/torch_orange_sad.png'),
+        happy: require('../mascot/orange/happy/torch_orange_happy.png'),
+        strong: require('../mascot/orange/strong/torch_orange_strong.png'),
+    },
+    blue: {
+        good: require('../assets/mascot/blue/good/torch_blue_good.png'),
+        sad: require('../../assets/mascot/orange/sad/torch_blue_sad.png'),
+        happy: require('../mascot/orange/happy/torch_blue_happy.png'),
+        strong: require('../mascot/orange/strong/torch_blue_strong.png'),
+    },
+    red: {
+        good: require('../assets/mascot/blue/good/torch_red_good.png'),
+        sad: require('../../assets/mascot/orange/sad/torch_red_sad.png'),
+        happy: require('../mascot/orange/happy/torch_red_happy.png'),
+        strong: require('../mascot/orange/strong/torch_red_strong.png'),
+    },
+    purple:{
+        good: require('../assets/mascot/blue/good/torch_purple_good.png'),
+        sad: require('../../assets/mascot/orange/sad/torch_purple_sad.png'),
+        happy: require('../mascot/orange/happy/torch_purple_happy.png'),
+        strong: require('../mascot/orange/strong/torch_purple_strong.png'),
+    },
+};
